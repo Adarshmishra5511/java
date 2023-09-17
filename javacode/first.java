faa267e8ccaf0febbc1afcf00ec0c18cc72916e9
Adarsh <adarshmishra5511@gmail.com>
2023-09-17 21:59:29 +0530

    hiii

--- javacode/first.java
+++
@@ -1,5 +0,0 @@
-class first{
-  public static void main(String[] args) {
-    System.err.println("hello");
-  }
-}
--- javacode/largestnum.java
+++
@@ -1,5 +0,0 @@
-//printing the largest num using scanner calss in array
-import java.util.*
-public class largestnum {
-  
-}
